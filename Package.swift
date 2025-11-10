c91862ad7597d0cca755e931806381d6e4403511
revesaurabhsystem2022 <revesaurabhsystem2022@gmail.com>
2025-11-10 10:32:03 +0530

    second_commit
    second_commit

--- Package.swift
+++ Package.swift
@@ -19,21 +19,21 @@ let package = Package(
         // Remote XCFramework - ReveChatSDK
         .binaryTarget(
             name: "ReveChatSDK_PBBinary",
-            url: "https://github.com/revesaurabhsystem2022/ReveChatSDK/releases/download/2.0.0/ReveChatSDK.xcframework.zip",
-            checksum: "5f3901ffe7fc80b43e38f6344e8259da912c5ef866e8f03fc30deff5e8facf10"
+            url: "https://github.com/revesaurabhsystem2022/ReveChatSDK_PB/releases/download/1.0.0/ReveChatSDK_PB.xcframework.zip",
+            checksum: "436c86c2baad4a3b1c29d88539842ec4b3cde8e65fa36df575566bdedeca2ce1"
         ),
 
         // Remote XCFramework - AFNetworking
         .binaryTarget(
             name: "AFNetworking",
-            url: "https://github.com/revesaurabhsystem2022/ReveChatSDK/releases/download/2.0.0/AFNetworking.xcframework.zip",
+            url: "https://github.com/revesaurabhsystem2022/ReveChatSDK_PB/releases/download/1.0.0/AFNetworking.xcframework.zip",
             checksum: "3210d9a5508febc628bdc611766c68870a2c1211261ecfb907db5220ac459f8a"
         ),
 
         // Remote XCFramework - WebRTC
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/revesaurabhsystem2022/ReveChatSDK/releases/download/2.0.0/WebRTC.xcframework.zip",
+            url: "https://github.com/revesaurabhsystem2022/ReveChatSDK_PB/releases/download/1.0.0/WebRTC.xcframework.zip",
             checksum: "0adcbddd9339ecdbb5905ea195710319c32e9e5f0a13075fb0a0ab8ae62265e4"
         ),
 
